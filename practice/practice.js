758c0246b9209dd6d5bcf0babd3107af438af407
Carolyn Shaffer <caroshaf@charter.net>
2021-03-21 14:22:45 -0500

    belly button bacteria study dashboard files

---
+++ practice/practice.js
@@ -0,0 +1,79 @@
+var trace = {
+    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+       "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: 'pie'
+  };
+  var data = [trace];
+  var layout = {
+    title: "'Pie' Chart",
+  };
+  Plotly.newPlot("plotArea", data, layout);
+
+
+  var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: 'lines',
+    type: 'scatter'
+  };
+  
+  var trace3 = {
+    x: [1, 2, 3, 4],
+    y: [12, 9, 15, 12],
+    mode: 'lines+markers',
+    type: 'scatter'
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  Plotly.newPlot('plotArea', data);
+
+  var numbers = [0,2,4,6,8];
+  var addFive = numbers.map(i => i + 5);
+console.log(addFive);
+
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityPops = cities.map(function(city){
+    return city.population;
+});
+console.log(cityPops);
+
+var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+var sAnimals = words.filter(s => String(s).startsWith("s"));
+console.log(sAnimals);
+
+var slice1 = words.slice(0,3);
+
+
+  ``
